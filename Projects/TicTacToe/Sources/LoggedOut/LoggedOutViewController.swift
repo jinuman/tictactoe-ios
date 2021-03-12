523e5e94ade85377fafe1c167e88da123e0ca6c3
Jinwoo Kim <jinumandev@gmail.com>
2021-03-13 01:27:21 +0900

    빈 LoggedOut RIBs 생성

---
+++ Projects/TicTacToe/Sources/LoggedOut/LoggedOutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoggedOutViewController.swift
+//  TicTacToe
+//
+//  Created by Jinwoo Kim on 2021/03/13.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol LoggedOutPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
+
+    weak var listener: LoggedOutPresentableListener?
+}
