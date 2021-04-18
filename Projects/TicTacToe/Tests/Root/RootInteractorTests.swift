20ddfd0c1a1ff29d8276e623cec966570f9b4144
Jinwoo Kim <jinumandev@gmail.com>
2021-04-18 17:27:17 +0900

    빈 RootInteractorTests 파일 작성

---
+++ Projects/TicTacToe/Tests/Root/RootInteractorTests.swift
@@ -0,0 +1,29 @@
+//
+//  RootInteractorTests.swift
+//  TicTacToeTests
+//
+//  Created by Jinwoo Kim on 2021/04/16.
+//
+
+@testable import TicTacToe
+import XCTest
+
+final class RootInteractorTests: XCTestCase {
+
+    private var interactor: RootInteractor!
+
+    // TODO: declare other objects and mocks you need as private vars
+
+    override func setUp() {
+        super.setUp()
+
+        // TODO: instantiate objects and mocks
+    }
+
+    // MARK: - Tests
+
+    func test_exampleObservable_callsRouterOrListener_exampleProtocol() {
+        // This is an example of an interactor test case.
+        // Test your interactor binds observables and sends messages to router or listener.
+    }
+}
