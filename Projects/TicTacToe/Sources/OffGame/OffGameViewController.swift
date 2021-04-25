5ab4cfd25667e0aa55d849d1ff9103b4386a58ad
Jinwoo Kim <jin35kim@gmail.com>
2021-04-25 16:04:42 +0900

    OffGameViewController UI 작성하고 플레이어 이름 표시

--- Projects/TicTacToe/Sources/OffGame/OffGameViewController.swift
+++ Projects/TicTacToe/Sources/OffGame/OffGameViewController.swift
@@ -36,6 +36,25 @@ final class OffGameViewController:
 
     // MARK: UI
 
+    private let player1Label = UILabel().then {
+        $0.font = UIFont.boldSystemFont(ofSize: 35)
+        $0.backgroundColor = .clear
+        $0.textColor = .blue
+        $0.textAlignment = .center
+    }
+    private let vsLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 25)
+        $0.backgroundColor = .clear
+        $0.textColor = .darkGray
+        $0.textAlignment = .center
+        $0.text = "vs"
+    }
+    private let player2Label = UILabel().then {
+        $0.font = UIFont.boldSystemFont(ofSize: 35)
+        $0.backgroundColor = .clear
+        $0.textColor = .red
+        $0.textAlignment = .center
+    }
     private let startButton = UIButton().then {
         $0.setTitle("Start Game", for: .normal)
         $0.setTitleColor(.white, for: .normal)
@@ -70,14 +89,39 @@ final class OffGameViewController:
                 self.listener?.startGame()
             })
             .disposed(by: self.disposeBag)
+
+        self.player1Label.text = self.player1Name
+        self.player2Label.text = self.player2Name
     }
 
 
     // MARK: - Layout
 
     override func configureLayout() {
-        self.view.addSubview(self.startButton)
+        [
+            self.player1Label,
+            self.vsLabel,
+            self.player2Label,
+            self.startButton
+        ]
+        .forEach {
+            self.view.addSubview($0)
+        }
 
+        self.player1Label.snp.makeConstraints {
+            $0.top.equalTo(self.view).offset(70)
+            $0.leading.trailing.equalTo(self.view).inset(20)
+            $0.height.equalTo(40)
+        }
+        self.vsLabel.snp.makeConstraints {
+            $0.top.equalTo(self.player1Label.snp.bottom).offset(10)
+            $0.leading.trailing.equalTo(self.player1Label)
+            $0.height.equalTo(20)
+        }
+        self.player2Label.snp.makeConstraints {
+            $0.top.equalTo(self.vsLabel.snp.bottom).offset(10)
+            $0.height.leading.trailing.equalTo(player1Label)
+        }
         self.startButton.snp.makeConstraints {
             $0.center.equalTo(self.view)
             $0.leading.trailing.equalTo(self.view).inset(40)
